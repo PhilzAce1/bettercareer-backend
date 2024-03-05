510b1a0db106c2ec4012c92c224a768d1cff7cee
fiyin-anne <fiyinakinsiku@gmail.com>
2024-03-05 01:51:53 +0100

    (chore) implement error handler

--- src/modules/users/oauth/strategy/google.ts
+++ src/modules/users/oauth/strategy/google.ts
@@ -1,5 +1,6 @@
 import { OAuth2Client, type TokenPayload } from 'google-auth-library';
 import { OAuthStrategy, SetOAuthUser } from './index.js';
+import CustomError from '../../../../helpers/error.js';
 
 export class GoogleOAuthStrategy implements OAuthStrategy {
   private payload: TokenPayload | undefined;
@@ -8,14 +9,22 @@ export class GoogleOAuthStrategy implements OAuthStrategy {
   private client: OAuth2Client;
 
   constructor(token?: string) {
-    if (!token) throw new TypeError();
+    if (!token) throw new CustomError({
+      message: 'OAuth token not found.',
+      statusCode: 404,
+      code: 'OAUTH ERROR'
+    });
 
     this.client = new OAuth2Client();
     this.token = token;
   }
 
   async serialize(setUser: SetOAuthUser) {
-    if (!this.payload) throw new TypeError();
+    if (!this.payload) throw new CustomError({
+      message: 'User data not found.',
+      statusCode: 404,
+      code: 'OAUTH ERROR'
+    });
     const payload = this.payload;
 
     setUser({
